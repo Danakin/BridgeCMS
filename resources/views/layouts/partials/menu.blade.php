9f61dd95856d49dbd708d7b4422f897938e47257
Danny Festor <danny@festor.info>
2021-06-04 08:24:40 +0900

    Update Menu Look

--- resources/views/layouts/partials/menu.blade.php
+++ resources/views/layouts/partials/menu.blade.php
@@ -1,10 +1,10 @@
-<nav class="fixed top-0 left-0 bottom-0 w-64 bg-green-900 text-white p-4">
+<nav class="fixed top-0 left-0 bottom-0 w-64 bg-green-900 text-white">
     <ul>
-        <li class="text-2xl font-bold text-center border-b-2 border-white">BridgeCMS</li>
-        <li><a href="{{ route('pages.show', 'home') }}">Home</a></li>
-        <li><a href="{{ route('pages.show', 'about-us') }}">About Us</a></li>
-        <li><a href="{{ route('pages.show', 'contact') }}">Contact</a></li>
-        <li><a href="{{ route('pages.show', 'blog') }}">Blog</a></li>
-        <li><a href="{{ route('pages.show', 'tutorials') }}">Tutorials</a></li>
+        <li class="text-2xl font-bold text-center border-b-2 border-white mt-4">BridgeCMS</li>
+        <li class="mt-4 pt-2 pb-2 hover:bg-green-700"><a class="block w-full pl-4" href="{{ route('pages.show', 'home') }}">Home</a></li>
+        <li class="py-2 hover:bg-green-700"><a class="block w-full pl-4" href="{{ route('pages.show', 'about-us') }}">About Us</a></li>
+        <li class="py-2 hover:bg-green-700"><a class="block w-full pl-4" href="{{ route('pages.show', 'contact') }}">Contact</a></li>
+        <li class="py-2 hover:bg-green-700"><a class="block w-full pl-4" href="{{ route('pages.show', 'blog') }}">Blog</a></li>
+        <li class="py-2 hover:bg-green-700"><a class="block w-full pl-4" href="{{ route('pages.show', 'tutorials') }}">Tutorials</a></li>
     </ul>
 </nav>
