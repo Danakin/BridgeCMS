5e9fde8f121616ca5dde89f6a9d5bf229d414944
Danny Festor <danny@festor.info>
2021-06-03 20:01:47 +0900

    Add first page test

---
+++ tests/Feature/PageTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Page;
+use App\Models\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PageTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_a_page_can_be_displayed()
+    {
+        $page = Page::factory()->has(Post::factory()->count(10))->create();
+
+        $response = $this->get(route('pages.show', $page));
+        $response->assertStatus(200);
+        $response->assertSee($page->title);
+        foreach ($page->posts as $post) {
+            $response->assertSee($post->title);
+        }
+        $response->assertViewHas('page');
+        $response->assertViewHas('posts');
+    }
+}
