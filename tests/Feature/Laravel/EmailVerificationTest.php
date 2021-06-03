284dd8b611393a78f3a74aa85ff4f9615f134634
Danny Festor <danny@festor.info>
2021-06-03 12:37:52 +0900

    Move Laravel Standard Feature Tests For Cleaner Folder

--- tests/Feature/Laravel/EmailVerificationTest.php
+++ tests/Feature/Laravel/EmailVerificationTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Laravel;
 
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
--- tests/Feature/Laravel/RegistrationTest.php
+++ tests/Feature/Laravel/RegistrationTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Laravel;
 
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
