61462c7045626b1a277ba61fed1b70b3f5f4d2fb
Danny Festor <danny@festor.info>
2021-06-04 09:05:45 +0900

    Improve Menu

--- resources/views/layouts/partials/menu.blade.php
+++ resources/views/layouts/partials/menu.blade.php
@@ -1,10 +1,22 @@
-<nav class="fixed top-0 left-0 bottom-0 w-64 bg-green-900 text-white">
+<nav class="fixed top-0 left-0 bottom-0 w-64 bg-green-900 text-white flex flex-col justify-between">
     <ul>
         <li class="text-2xl font-bold text-center border-b-2 border-white mt-4">BridgeCMS</li>
-        <li class="mt-4 pt-2 pb-2 hover:bg-green-700"><a class="block w-full pl-4" href="{{ route('pages.show', 'home') }}">Home</a></li>
-        <li class="py-2 hover:bg-green-700"><a class="block w-full pl-4" href="{{ route('pages.show', 'about-us') }}">About Us</a></li>
-        <li class="py-2 hover:bg-green-700"><a class="block w-full pl-4" href="{{ route('pages.show', 'contact') }}">Contact</a></li>
-        <li class="py-2 hover:bg-green-700"><a class="block w-full pl-4" href="{{ route('pages.show', 'blog') }}">Blog</a></li>
-        <li class="py-2 hover:bg-green-700"><a class="block w-full pl-4" href="{{ route('pages.show', 'tutorials') }}">Tutorials</a></li>
+        @can('access-admin')
+            <x-menu.item route="" title="Admin Panel" class="mt-4 border-t-2 border-b-2 border-green-700"></x-menu.item>
+        @endcan
+        <x-menu.item route="{{ route('pages.show', 'home') }}" title="Home" class="mt-4"></x-menu.item>
+        <x-menu.item route="{{ route('pages.show', 'about-us') }}" title="About Us"></x-menu.item>
+        <x-menu.item route="{{ route('pages.show', 'contact') }}" title="Contact"></x-menu.item>
+        <x-menu.item route="{{ route('pages.show', 'blog') }}" title="Blog"></x-menu.item>
+        <x-menu.item route="{{ route('pages.show', 'tutorials') }}" title="Tutorials"></x-menu.item>
     </ul>
+    @auth
+    <section class="d-flex flex-col border-t-2 border-white">
+        <article class="text-center py-2">{{ auth()->user()->email }}</article>
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button class="py-2 w-full hover:bg-green-700">{{__('Logout')}}</button>
+        </form>
+    </section>
+    @endauth
 </nav>
